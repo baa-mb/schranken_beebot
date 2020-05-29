9e98202582ac23d7d9bc658ebff04561d1381e5f
baa <34665120+baa-mb@users.noreply.github.com>
2020-05-29 19:42:27 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 function schranken (flag: boolean) {
     if (flag != schranken_offen) {
+        basic.showNumber(-1)
         if (flag) {
             strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
             strip.show()
@@ -10,12 +11,15 @@ function schranken (flag: boolean) {
             strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
             strip.show()
             basic.pause(500)
-            pins.servoWritePin(AnalogPin.P0, offen)
+            pins.servoWritePin(AnalogPin.P12, offen)
         } else {
+            strip.showColor(neopixel.colors(NeoPixelColors.Yellow))
+            strip.show()
+            basic.pause(2000)
             strip.showColor(neopixel.colors(NeoPixelColors.Red))
             strip.show()
-            basic.pause(1000)
-            pins.servoWritePin(AnalogPin.P0, zu)
+            basic.pause(2000)
+            pins.servoWritePin(AnalogPin.P12, zu)
         }
         schranken_offen = flag
     }
@@ -27,32 +31,32 @@ let schranken_offen = false
 let offen = 0
 let zu = 0
 zu = 180
-offen = 0
+offen = 80
 schranken_offen = false
-pins.servoWritePin(AnalogPin.P0, zu)
+pins.servoWritePin(AnalogPin.P12, zu)
 let fehler = 0
-strip = neopixel.create(DigitalPin.P12, 3, NeoPixelMode.RGB)
+strip = neopixel.create(DigitalPin.P1, 3, NeoPixelMode.RGB)
 strip.clear()
 strip.show()
 basic.pause(500)
 basic.forever(function () {
     basic.clearScreen()
+    basic.pause(500)
     distanz = sonar.ping(
-    DigitalPin.P1,
     DigitalPin.P2,
+    DigitalPin.P8,
     PingUnit.Centimeters
     )
-    if (distanz < 15) {
-        basic.showNumber(distanz)
+    basic.showNumber(distanz)
+    if (distanz < 10) {
         if (distanz < 5) {
             schranken(true)
         } else {
             schranken(false)
         }
     } else {
-        strip.showRainbow(1, 360)
-        strip.rotate(1)
-        strip.show()
-        basic.pause(1000)
+        if (schranken_offen) {
+            schranken(false)
+        }
     }
 })
