56193e090e1d14ae64e69c67ef6c10d1e8c76adb
baa <34665120+baa-mb@users.noreply.github.com>
2020-05-30 23:57:11 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 function schranken (flag: boolean) {
     if (flag != schranken_offen) {
-        basic.showNumber(-1)
+        basic.showIcon(IconNames.No)
         if (flag) {
             strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
             strip.show()
@@ -11,7 +11,7 @@ function schranken (flag: boolean) {
             strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
             strip.show()
             basic.pause(500)
-            pins.servoWritePin(AnalogPin.P12, offen)
+            pins.servoWritePin(AnalogPin.P0, offen)
         } else {
             strip.showColor(neopixel.colors(NeoPixelColors.Yellow))
             strip.show()
@@ -19,44 +19,39 @@ function schranken (flag: boolean) {
             strip.showColor(neopixel.colors(NeoPixelColors.Red))
             strip.show()
             basic.pause(2000)
-            pins.servoWritePin(AnalogPin.P12, zu)
+            pins.servoWritePin(AnalogPin.P0, zu)
         }
         schranken_offen = flag
+        basic.pause(1000)
     }
-    basic.pause(1000)
 }
 let distanz = 0
 let strip: neopixel.Strip = null
 let schranken_offen = false
 let offen = 0
 let zu = 0
+let fehler = 0
 zu = 180
 offen = 80
 schranken_offen = false
-pins.servoWritePin(AnalogPin.P12, zu)
-let fehler = 0
+pins.servoWritePin(AnalogPin.P0, zu)
 strip = neopixel.create(DigitalPin.P1, 3, NeoPixelMode.RGB)
 strip.clear()
 strip.show()
 basic.pause(500)
 basic.forever(function () {
     basic.clearScreen()
-    basic.pause(500)
+    basic.pause(1000)
     distanz = sonar.ping(
     DigitalPin.P2,
     DigitalPin.P8,
     PingUnit.Centimeters
     )
-    basic.showNumber(distanz)
-    if (distanz < 10) {
-        if (distanz < 5) {
-            schranken(true)
-        } else {
-            schranken(false)
-        }
+    if (distanz < 5) {
+        schranken(true)
     } else {
-        if (schranken_offen) {
-            schranken(false)
-        }
+        schranken(false)
+        fehler += 1
     }
+    basic.showNumber(fehler)
 })
